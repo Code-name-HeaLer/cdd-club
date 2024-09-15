a29f924fbba4b150e5cd68b4b5f644881f6bb5cc
CodeNameHealer <swagatprasad3344@gmail.com>
2024-09-15 22:01:07 +0530

    program section fixed

--- src/Components/Programs/Programs.jsx
+++ src/Components/Programs/Programs.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Programs.css'
 import program_1 from '../../assets/program-1.avif'
-import program_2 from '../../assets/program-2.avif'
+import program_2 from '../../assets/program-2.jpg'
 import program_3 from '../../assets/program-3.avif'
+import program_4 from '../../assets/program-4.jpg'
 import program_icon_1 from '../../assets/www.png'
 import program_icon_2 from '../../assets/app1.png'
 import program_icon_3 from '../../assets/ML.png'
+import program_icon_4 from '../../assets/game-icon.png'
+
 
 
 const Programs = () => {
@@ -30,6 +33,13 @@ const Programs = () => {
                 <div className="caption">
                     <img src={program_icon_3} alt="" />
                     <p>AI/ML</p>
+                </div>
+            </div>
+            <div className="program">
+                <img src={program_4} alt="" />
+                <div className="caption">
+                    <img src={program_icon_4} alt="" />
+                    <p>Game Development</p>
                 </div>
             </div>
         </div>
