c62c19327b0bd5a025539a4ec6c6c82d311691f7
CodeNameHealer <swagatprasad3344@gmail.com>
2024-09-15 21:28:27 +0530

    about section updated

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './About.css'
-import about_img from '../../assets/about.png'
+import about_img from '../../assets/club-logo.jpeg'
 import play_icon from '../../assets/play-icon.png'
 
 const About = ({setPlayState}) => {
@@ -14,21 +14,24 @@ const About = ({setPlayState}) => {
             </div>
             <div className="about-right">
                 <h3>ABOUT OUR CLUB</h3>
-                <h2>Your all in one stop for everything TECH</h2>
-                <p>Embark on a transformative educational journey with our 
-                、EDUs university's comprehensive education programs. 
-                Our cutting-edge curriculum is designed to empower students 
-                with the knowledge, asset inents skills, and experiences needed 
-                to excel in the dynamic field of education</p>
-                <p>With a focus on innovation, hands-on learning, 
-                and personalized mentorship, our programs prepare 
-                aspiring educators to make meaningful impact in classrooms, 
-                schools, and communities</p>
-                <p>Whether you aspire to become a teacher, administrator, 
-                Titie css counselor, or educational leader, our diverse 
-                range of programs offers the perfect pathway to achieve 
-                your goals and unlock your full potential in shaping the 
-                future of education </p>
+                <h2>All Things Tech, All in One Place</h2>
+                <p>At CDD Club, embark on a transformative 
+                    journey into the world of technology and 
+                    creativity. Our vibrant community of developers, 
+                    designers, and tech enthusiasts offers a 
+                    comprehensive program designed to equip you with 
+                    the knowledge, skills, and experiences needed to 
+                    excel in the dynamic field of coding, design, and 
+                    development.</p>
+                <p>With a focus on hands-on learning, collaboration, and 
+                    mentorship, our programs provide opportunities to engage in 
+                    workshops, work on innovative projects, build a strong network, 
+                    and enhance your technical abilities.</p>
+                <p>Our mission is to foster an inclusive environment where creativity 
+                    meets technology. Whether you're a seasoned professional or just 
+                    starting out, CDD Club provides the tools, resources, and community 
+                    support to help you thrive in the fast-evolving world of coding and 
+                    design.</p>
             </div>
         </div>
     )
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,19 +5,16 @@ import dark_arrow from '../../assets/dark-arrow.png'
 const Hero = () => {
 
   const handleClick = () => {
-    window.location.href = 'https://google.com'; // Replace with your desired URL
+    window.location.href = 'https://linktr.ee/CDD_Club'; // Replace with your desired URL
   };
 
   return (
     <div className='hero container'>
         <div className="hero-text">
             <h1>Where Code Meets Creativity, Magic Happens</h1>
-            <p>At CDD Club, we are passionate about pushing the 
-              boundaries of technology and creativity. Our club 
-              is a vibrant community of developers, designers, 
-              and tech enthusiasts who come together to explore 
-              the latest trends, tools, and techniques in coding, 
-              design, and development.   
+            <p>CDD Club is the place where Innovation Meets Community. 
+              Join us in a dynamic space where tech enthusiasts 
+              collaborate, explore, and create the future.  
             </p>
             <button onClick={handleClick} className='btn'>Explore More <img src={dark_arrow} alt="" /></button>
         </div>
