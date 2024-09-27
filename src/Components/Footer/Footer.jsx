833ca4fbd1847c36dcdac38963c9d7efcbf1372b
CodeNameHealer <swagatprasad3344@gmail.com>
2024-09-27 19:11:33 +0530

    footer updated with icons

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "./Footer.css";
 
+// icons
+import insta from '../../assets/footer-icons/instagram.png'
+import linkedin from '../../assets/footer-icons/linkedin.png'
+import twitter from '../../assets/footer-icons/twitter.png'
+import github from '../../assets/footer-icons/github.png'
+
 const Footer = () => {
     return (
         <div className="footer">
             <p>&copy; 2024 CDD Club PMEC. All rights reserved</p>
             <ul>
-                <li>Terms of Services</li>
-                <li>Privacy policy</li>
+                <a href="https://www.instagram.com/cdd_club_pmec/"><li><img src={insta} alt="" /></li></a>
+                <a href="https://www.linkedin.com/company/coding-design-and-development/"><li><img src={linkedin} alt="" /></li></a>
+                <a href="https://x.com/cddclubpmec"><li><img src={twitter} alt="" /></li></a>
+                <a href="https://github.com/CodingClubPMEC"><li><img src={github} alt="" /></li></a>
             </ul>
         </div>
     );
