4c460cef255f1084a00ab9fc053f62c8a30b6dd0
CodeNameHealer <swagatprasad3344@gmail.com>
2024-09-27 17:16:34 +0530

    images added

--- src/Components/Campus/Campus.jsx
+++ src/Components/Campus/Campus.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './Campus.css'
-import gallery_1 from '../../assets/gallery-1.png'
-import gallery_2 from '../../assets/gallery-2.png'
-import gallery_3 from '../../assets/gallery-3.png'
-import gallery_4 from '../../assets/gallery-4.png'
+import gallery_1 from '../../assets/pic-1.jpeg'
+import gallery_2 from '../../assets/pic-2.jpeg'
+import gallery_3 from '../../assets/pic-3.jpeg'
+import gallery_4 from '../../assets/pic-4.jpeg'
 import white_arrow from '../../assets/white-arrow.png'
 
 const Campus = () => {
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -10,15 +10,15 @@ const Hero = () => {
 
   return (
     <div className='hero container'>
-        <div className="hero-text">
-            <h1>Where Code Meets Creativity, Magic Happens</h1>
-            <p>CDD Club is the place where Innovation Meets Community. 
-              Join us in a dynamic space where tech enthusiasts 
-              collaborate, explore, and create the future.  
-            </p>
-            <button onClick={handleClick} className='btn'>Explore More <img src={dark_arrow} alt="" /></button>
-        </div>
-      
+      <div className="hero-text">
+        <h1>When Code Meets Creativity, Magic Happens</h1>
+        <p>CDD Club is the place where Innovation Meets Community.
+          Join us in a dynamic space where tech enthusiasts
+          collaborate, explore, and create the future.
+        </p>
+        <button onClick={handleClick} className='btn'>Explore More <img src={dark_arrow} alt="" /></button>
+      </div>
+
     </div>
   )
 }
